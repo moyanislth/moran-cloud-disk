b5197cf15892a80c29bf88c3e0de46cfef4a22ba
Myan <3094673523@qq.com>
2025-11-08 14:05:44 +0800

    Initialize the front-end and back-end environments

---
+++ moran-backend/src/main/java/com/lth/moran/MoranApplication.java
@@ -0,0 +1,16 @@
+package com.lth.moran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Myan
+ */
+@SpringBootApplication
+public class MoranApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoranApplication.class, args);
+    }
+
+}
---
+++ moran-backend/src/test/java/com/lth/moran/MoranApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lth.moran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoranApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
